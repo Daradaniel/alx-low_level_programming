4c0947d41053cf876584314ff7b5a9658ffeeb03
Daradaniel <daviddaradaniel@gmail.com>
2022-09-09 19:11:40 -0700

    eighth commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all numbers of base 16 in lower case,
- *        only using putchar.
+ * main - Prints all numbers of base 16 in lower case.
  * 
  * Return: Always 0.
  */

173e0d935ed8fa33ceb1fa3759afd4f6e252a04c
Daradaniel <daviddaradaniel@gmail.com>
2022-09-26 11:23:14 -0700

    0

--- 0x07-pointers_arrays_strings/main/0-memset.c
+++ 0x07-pointers_arrays_strings/main/0-memset.c
@@ -3,8 +3,8 @@
 /**
  * memset - Fills the first n bytes of the memory area
  *          pointed to by @s with the constant byte @c.
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
+ * @dest: A pointer to the memory area to be filled.
+ * @src: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
  *
  * Return: A pointer to the filled memory area @s.

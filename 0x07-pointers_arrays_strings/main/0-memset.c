97ad92903f03a1e027b1dfd1b498fc2feb891649
Daradaniel <daviddaradaniel@gmail.com>
2022-09-26 11:26:49 -0700

    0

--- 0x07-pointers_arrays_strings/main/0-memset.c
+++ 0x07-pointers_arrays_strings/main/0-memset.c
@@ -11,12 +11,14 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int index;
+unsigned int i;
 
-for (index = 0; n > 0; index++)
+/*Delacring FOR*/
+for (i = 0; i < n; i++)
 {
-dest[index] = src;
-n -= 1;
-}
+*(dest + i) = src; /*add 1 position s*/
+
+} /*END FOR*/
+
 return (dest);
 }
